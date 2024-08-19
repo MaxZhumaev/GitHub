1824ea20bd00c10d1e2883a32fa7e5f65ae27a0f
MaxZhumaev <max-zhumaev@ya.ru>
2024-08-19 21:26:42 +0300

    Create mudule_3_1.py

---
+++ mudule_3_1.py
@@ -0,0 +1,26 @@
+def count_calls():
+  global calls
+  calls += 1
+
+def string_info(Input_string):
+    count_calls()
+    return (len(Input_string), 
+        str(Input_string).upper(),
+        str(Input_string).lower())
+
+def is_contains(search_string, search_list):
+    count_calls()
+    for i in range(0, len(search_list)):
+        result = False
+        if str(search_string).upper() in str(search_list[i]).upper():
+            result = True
+            break
+    return result
+
+calls = 0
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'urBAN', 'BaNaN']))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+print(is_contains('cycle', ['recycling', 'cyclic']))
+print(calls)

eced781b56138964d21bed2ab08683e405851cf4
MaxZhumaev <max-zhumaev@ya.ru>
2024-08-30 17:55:59 +0300

    Add files via upload

---
+++ module_4_2.py
@@ -0,0 +1,8 @@
+def test_function():
+    def inner_function():
+        print("Я в области видимости функции test_function")
+# Вызовите функцию inner_function внутри функции test_function
+    inner_function()
+test_function()
+# Попробуйте вызывать inner_function вне функции test_function и посмотрите на результат выполнения программы
+inner_function()
